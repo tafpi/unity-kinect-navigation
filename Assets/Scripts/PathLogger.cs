490574145807958e2038b4ac29fd9a8f9dcb93e9
tafpi <tilpoth@gmail.com>
2019-06-04 00:42:33 +0300

    path logger done

--- Assets/Scripts/PathLogger.cs
+++ Assets/Scripts/PathLogger.cs
@@ -7,34 +7,53 @@ using System.IO;
 
 public class PathLogger : MonoBehaviour
 {
+    public GameObject player;
 
-    string FILE_NAME = "positionFile.txt";
+    string fileName = "positionFile";
     int time = 0;
-    int interval = 150;
+    int interval = 20;
+    float x;
+    float z;
+    string polylineCoordinates = "";
+    StreamWriter sw;
+    public int fileCount;
+    string fmt = "0000.##";
+
     // Use this for initialization
     void Start()
-    {
+    {        
+        fileCount = 0;
+        do
+        {
+            fileCount++;
+        }
+        while (File.Exists(fileName + (fileCount > 0 ? "_" + fileCount.ToString(fmt) : "")+ ".svg"));
+        
+        sw = File.AppendText(fileName + (fileCount > 0 ? "_" + fileCount.ToString(fmt) : "") + ".svg");
+        sw.WriteLine("<!DOCTYPE svg PUBLIC ' -//W3C//DTD SVG 1.1//EN' 'http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd'><svg version = '1.1' xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' xml:space='preserve' width='1000' height='1000'><polyline points='");
+    }
 
+    void OnDestroy()
+    {
+        sw.WriteLine(polylineCoordinates+"' fill='white' stroke='#000000' stroke-width='3' /></svg>");
+        sw.Close();
     }
 
     // Update is called once per frame
     void Update()
     {
-
         if (time > interval)
         {
-            float x = transform.position.x;
-            float z = transform.position.z;
-            float fx = transform.forward.x;
-            float fz = transform.forward.z;
-            StreamWriter sw = File.AppendText(FILE_NAME);
-            sw.WriteLine("my  position is " + x + " my z position is " + z);
-            sw.WriteLine("I'm facing " + transform.forward);
-            sw.Close();
-            Debug.Log("write to file");
+            x = -player.transform.position.x * 10 + 400;
+            z = player.transform.position.z * 10 + 400;
+            //sw.Write("" + x + ", " + z + " ");
+            polylineCoordinates += "" + x + ", " + z + " ";
+            //Debug.Log("write to file");
             time = 0;
         }
         else
+        {
             time++;
+        }
     }
 }
