af10b88cc27a4033f445644dfc41cbf4dead6eef
tafpi <tilpoth@gmail.com>
2019-06-09 01:08:33 +0300

    rotate shoulders done

--- Assets/Scripts/Rotate Gestures/GestureRotateShoulders.cs
+++ Assets/Scripts/Rotate Gestures/GestureRotateShoulders.cs
@@ -11,33 +11,28 @@ public class GestureRotateShoulders : MonoBehaviour
 
     public float gestureRate;
 
-    public float slope = 2.5f;
+    public float slope = 4;
     public float minimumRate = 0.1f;
-    public float maximumRate = 0.2f;
+    public float maximumRate = 0.4f;
 
     private Vector3 shoulderLeft;
     private Vector3 shoulderRight;
-    //private Vector3 hipLeft;
-    //private Vector3 hipRight;
     private Vector3 ankleLeft;
     private Vector3 ankleRight;
 
     private Vector3 horizontalShoulders;
-    //private Vector3 horizontalHips;
     private Vector3 horizontalAnkles;
-    private float direction;
+    public float direction;
     public float rate;
     
     private GestureState state;
-
-    // Start is called before the first frame update
+    
     void Start()
     {
         state = gameObject.GetComponent<GestureState>();
         trackGesture = gameObject.GetComponent<GestureState>().gestureTracked;
     }
-
-    // Update is called once per frame
+    
     void Update()
     {
         if (trackGesture)
@@ -70,46 +65,20 @@ public class GestureRotateShoulders : MonoBehaviour
                 {
                     shoulderLeft = Functions.unityVector3(body.Joints[JointType.ShoulderLeft].Position);
                     shoulderRight = Functions.unityVector3(body.Joints[JointType.ShoulderRight].Position);
-                    //hipLeft = Functions.unityVector3(body.Joints[JointType.HipLeft].Position);
-                    //hipRight = Functions.unityVector3(body.Joints[JointType.HipRight].Position);
                     ankleLeft = Functions.unityVector3(body.Joints[JointType.AnkleLeft].Position);
                     ankleRight = Functions.unityVector3(body.Joints[JointType.AnkleRight].Position);
 
                     horizontalShoulders = shoulderLeft - shoulderRight;
-                    horizontalShoulders.y = 0;
-
-                    //horizontalHips = hipLeft - hipRight;
-                    //horizontalHips.y = 0;
-
+                    horizontalShoulders = new Vector3(horizontalShoulders.x, 0, horizontalShoulders.z);
                     horizontalAnkles = ankleLeft - ankleRight;
-                    horizontalAnkles.y = 0;
-                    //horizontalAnkles.z = 0;
-                    //if(ankleLeft.z > ankleRight.z)
-                    //{
-                    //    horizontalAnkles.z = horizontalAnkles.z - (Mathf.Abs(ankleLeft.z - ankleRight.z)/2);
-                    //}
-                    //else
-                    //{
-                    //    horizontalAnkles.z = horizontalAnkles.z - (Mathf.Abs(ankleLeft.z - ankleRight.z)/2);
-                    //}
-                    horizontalAnkles.z = horizontalAnkles.z / 2;
-                    Debug.Log(horizontalAnkles.z);
-
-
-                    //directionTop = Mathf.Sign(((hipLeft - hipRight) - (shoulderLeft - shoulderRight)).z);
-                    //rateTop = Mathf.Sin(Vector3.Angle(horizontalHips, horizontalShoulders) * Mathf.Deg2Rad) * directionTop;
-                    //directionBot = Mathf.Sign(((ankleLeft - ankleRight) - (hipLeft - hipRight)).z);
-                    //rateBot = Mathf.Sin(Vector3.Angle(horizontalAnkles, horizontalHips) * Mathf.Deg2Rad) * directionTop;
+                    horizontalAnkles = new Vector3(horizontalAnkles.x, 0, horizontalAnkles.z * 0.05f);
 
                     direction = Mathf.Sign((horizontalAnkles - (shoulderLeft - shoulderRight)).z);
 
                     rate = Mathf.Sin(Vector3.Angle(horizontalAnkles, horizontalShoulders) * Mathf.Deg2Rad);
 
-
-                    // SAFE : vector left
-
-                    //rate = Functions.limitValue(minimumRate, maximumRate, rate);
-                    //gestureRate = Mathf.Pow((rate - minimumRate) / (maximumRate - minimumRate), slope) * direction;
+                    rate = Functions.limitValue(minimumRate, maximumRate, rate);
+                    gestureRate = Mathf.Pow((rate - minimumRate) / (maximumRate - minimumRate), slope) * direction;
 
                     if (state != null) state.gestureRate = gestureRate;
 
@@ -119,17 +88,5 @@ public class GestureRotateShoulders : MonoBehaviour
             }
         }
     }
-
-    //private float rateNorm(float rate, float min, float max, float slope)
-    //{
-    //    rate = Functions.limitValue(minimumRate, maximumRate, rate);
-    //    return Mathf.Pow((rate - min) / (max - min), slope);
-    //    //if ( (rate >= min) && (rate <= max) ){
-    //    //    //return Mathf.Pow((rate - min / max) / (1 - (min / max)), slope);
-    //    //    //return Mathf.Pow((rate*max - min)/(max-min), slope);
-    //    //} else {
-    //    //    return 0;
-    //    //}
-    //}
 
 }
