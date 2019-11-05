f074d99291dedd2e4594ef5c59d685a7b27c5c38
tafpi <tilpoth@gmail.com>
2019-11-06 01:10:10 +0200

    added BezierSolution plugin, moved plugins to Plugins folder, deviation scripts. to-do: run logs

---
+++ Assets/Scripts/LogRun.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LogRun : MonoBehaviour
+{
+    // Attach as component to player.
+}
---
+++ Assets/Scripts/Path Deviation/RepeatAlongBezier.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BezierSolution
+{
+    [ExecuteInEditMode]
+    public class RepeatAlongBezier : MonoBehaviour
+    {
+        public BezierSpline spline;
+        public GameObject deviationCollider;
+        public GameObject placeholder;
+
+        bool refreshing = false;
+        int n = 5;
+        GameObject collidersParent;
+
+        private void OnEnable()
+        {
+            collidersParent = Instantiate(placeholder, transform);
+            for (int i = 0; i < n; i++)
+            {
+                Instantiate(deviationCollider, spline.GetPoint((float)i / (n - 1)), deviationCollider.transform.rotation, collidersParent.transform);
+                //if (i == 4)
+                //{
+                //    refreshing = false;
+                //}
+            }
+        }
+
+        private void OnDisable()
+        {
+            DestroyImmediate(collidersParent);
+            //foreach (Transform child in placeholder.transform)
+            //{
+            //    GameObject.DestroyImmediate(child.gameObject);
+            //}
+        }
+
+        void Refresh()
+        {
+            if (placeholder.transform.childCount == n)
+            {
+                foreach (Transform child in placeholder.transform)
+                {
+                    Destroy(child.gameObject);
+                }
+            }
+            if (placeholder.transform.childCount == 0)
+            {
+                //refreshing = true;
+                for (int i = 0; i < n; i++)
+                {
+                    Instantiate(deviationCollider, spline.GetPoint((float)i / (n-1)), deviationCollider.transform.rotation, placeholder.transform);
+                    //if (i == 4)
+                    //{
+                    //    refreshing = false;
+                    //}
+                }
+            }
+        }
+        
+    }
+
+}
